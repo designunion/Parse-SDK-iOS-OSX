874b78880361a6a01d963f0524cd93db88f6cd1a
designunion <brian@designunion.me>
2018-12-06 16:36:26 -0800

    Update PFObject.m

--- Parse/Parse/PFObject.m
+++ Parse/Parse/PFObject.m
@@ -765,7 +765,6 @@ static void PFObjectAssertValueIsKindOfValidClass(id object) {
  Checks if Parse class name could be used to initialize a given instance of PFObject or it's subclass.
  */
 + (void)_assertValidInstanceClassName:(NSString *)className {
-    NSLog(@"%@",className);
     
     PFParameterAssert(className, @"Class name can't be 'nil'.");
     
